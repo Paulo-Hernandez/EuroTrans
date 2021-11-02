a30c9532bf6db583342e6abf78b7360ca3984777
JotaPeGH <83192365+JotaPeGH@users.noreply.github.com>
2021-11-02 17:48:40 -0300

    v0.11 navbar

--- index.php
+++ index.php
@@ -1,17 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-    <link rel="stylesheet" href="./css/bootstrap.min.css"/>
-</head>
-<body>
-    <from action="validar.php" method="post">
-    <h1>Login</h1>
-    <p>Usuario <input type="text" placeholder="ingrese su usuario" name="usuario"></p>
-    <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
-    <input type="submit" value="Ingresar">
-</body>
-</html>
+<?php include("template/cabecera.php"); ?>
+    
+    <div class="jumbotron">
+        <h1>Bienvenido Usuario</h1>
+        <h6>Esta es la pagina donde podra acceder a la base de datos de Eurotrans</h6>
+    </div>
+    <br>
+    <div class="container">
+        <from action="validar.php" method="post">
+        <h1>Login</h1>
+        <p>Usuario <input type="text" placeholder="ingrese su usuario" name="usuario"></p>
+        <p>Contraseña <input type="password" placeholder="ingrese su contraseña" name="contraseña"></p>
+        <input type="submit" value="Ingresar">
+    </div>
+    
--- template/cabecera.php
+++ template/cabecera.php
@@ -1,28 +1,22 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Login</title>
     <link rel="stylesheet" href="./css/bootstrap.min.css"/>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <ul class="nav navbar-nav">
             <li class="nav-item">
-                <a class="nav-link" href="#">Logo</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Inicio</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Servicios</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Clientes</a>
+                <a class="nav-link" href="https://eurotranschile.cl/">
+                    <img src="img/logo-eurotrans-1.png">
+                </a>
             </li>
         </ul>
     </nav>
-    <div class="container">
-        <div class="container">
+    <br>
+</body>
+</html>
