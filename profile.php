1d5a7065aa2c5a549ce94f272d6d89d05864e6eb
Kalcio <mmunozm@utem.cl>
2021-11-03 14:40:42 -0300

    profile.php llenado

--- profile.php
+++ profile.php
@@ -9,13 +9,13 @@ if (!isset($_SESSION['loggedin'])) {
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
 $DATABASE_PASS = '';
-$DATABASE_NAME = 'phplogin';
+$DATABASE_NAME = 'ventas';
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
 if (mysqli_connect_errno()) {
 	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
 }
 // We don't have the password or email info stored in sessions so instead we can get the results from the database.
-$stmt = $con->prepare('SELECT password, email FROM accounts WHERE id = ?');
+$stmt = $con->prepare('SELECT password, email FROM users WHERE id = ?');
 // In this case we can use the account ID to get the account info.
 $stmt->bind_param('i', $_SESSION['id']);
 $stmt->execute();
@@ -23,6 +23,8 @@ $stmt->bind_result($password, $email);
 $stmt->fetch();
 $stmt->close();
 ?>
+
+<?php include("template/cabecera.php"); ?>
 
 <!DOCTYPE html>
 <html>
@@ -38,25 +40,25 @@ $stmt->close();
 		<nav class="navtop">
 			<div>
 				<h1>Website Title</h1>
-				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
-				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Perfil</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Cerrar sesión</a>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Profile Page</h2>
+			<h2>Mi perfil</h2>
 			<div>
-				<p>Your account details are below:</p>
+				<p>Detalle de la cuenta:</p>
 				<table>
 					<tr>
-						<td>Username:</td>
+						<td>Usuario:</td>
 						<td><?=$_SESSION['name']?></td>
 					</tr>
 					<tr>
-						<td>Password:</td>
+						<td>Contraseña:</td>
 						<td><?=$password?></td>
 					</tr>
 					<tr>
-						<td>Email:</td>
+						<td>Correo:</td>
 						<td><?=$email?></td>
 					</tr>
 				</table>
