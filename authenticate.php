8538e70473a0458a96d2fe6a95b9ad01341a2f7d
Kalcio <mmunozm@utem.cl>
2021-11-06 02:20:04 -0300

    Verificación de contraseña codificada

--- authenticate.php
+++ authenticate.php
@@ -25,10 +25,7 @@ if($stmt = $con->prepare('SELECT id, password FROM users WHERE username = ?')){
         $stmt->bind_result($id, $password);
         $stmt->fetch();
 
-        //if (password_verify($_POST['password'], $password)
-        if ($_POST['password'] === $password){
-        
-
+        if(password_verify($_POST['password'], $password)){
             session_regenerate_id();
             $_SESSION['loggedin'] = TRUE;
             $_SESSION['name'] = $_POST['username'];
